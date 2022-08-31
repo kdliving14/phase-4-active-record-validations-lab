96128819ed048167a21157c4a969c66c29f12a6f
Karter Livingston <kdlmuzixnerd14@gmail.com>
2022-08-31 09:00:20 -0700

    commit tree f12aacf43f143d6438f66a21763a308fd5192807

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    # variable ^      ^is there    ^and   ^is not the same as anything else
+    validates :phone_number, length: { is: 10 }
+    # variable ^                ^is exactly ^ long
 end
